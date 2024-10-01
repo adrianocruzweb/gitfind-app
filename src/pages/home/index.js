8f4b4d8edb6d5ed8e145a95492159e59da7268b0
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-10-01 16:01:52 -0300

    css style initial

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,31 @@
+import {Header} from '../../components/Header';
+import background from '../../assets/background.png';
+import './styles.css';
 
 const App = () => {
   return (
     <div className="App">
-      aqui
+      <Header/>
+      <div className="conteudo">
+        <img src={background} className="background" alt="background" />
+        <div className="info">
+          <div>
+            <input name="usuario" placeholder="@username"/>
+            <button>Buscar</button>
+          </div>
+          <div>
+            <img
+              src="https://avatars.githubusercontent.com/u/17552862?v=4"
+              className="profile"
+              alt='imagem do perfil'
+            />
+            <div>
+              <h3>Adriano Cruz</h3>
+              <p>Descrição</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
